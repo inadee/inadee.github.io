83a0df1e9435ecaa6125f950888e9a1ecc5ff314
Paul Evans <paul.d.evans@gmail.com>
2020-08-17 20:47:39 -0500

    Updated deploy to commit and push source files too

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,11 @@
+pushd %~dp0
+git add *.md
+git add *.png
+git add *.jpg
+git add *.gif
+git add *.toml
+git commit -m "Committing and pushing source files" && git push origin main
+popd
 pushd %~dp0\public
 git add . && git commit -m "Publishing to gh-pages" && git push origin gh-pages
 popd
