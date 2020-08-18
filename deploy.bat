d075ca43b4dfcb53f65acab67732b7227bcfc176
Paul Evans <paul.d.evans@gmail.com>
2020-08-17 20:42:02 -0500

    fixed deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
-pushd %~dp0
-git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+pushd %~dp0\public
+git add . && git commit -m "Publishing to gh-pages" && git push origin gh-pages
 popd
